702a2f944146c74cf97d69fd303a4a613ed74b72
binary10ve <sanil.m.naik@gmail.com>
2012-11-09 12:50:26 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OverTheWeekend::Application.config.secret_token = '1ab8340ab5a70d8eeccece07a7aaca19f89ab938ed0196e368d0c503d1b8344d97fcb4e826b417417eedc6181803e23be0596dfd83da97f2ec289d54da6f6058'
